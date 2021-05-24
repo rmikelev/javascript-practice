fcd9c772f0d184759beb9a899bae3ddefbf01c5d
rmikelev <rmikelev1@comcast.net>
2021-05-24 11:52:44 -0700

    finished day2 homework console log number, Bigint and typeof to confirm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+
+
+const str = 'Supercalifragilisticexpialidocious'
+    let n = str.length;
+    console.log(n)
+
+
+let number = 100
+console.log(number);
+
+console.log(typeof 100);
+
+
+const bigint = 1234567890123456789012345678901234567890n;
+console.log(bigint);
+ 
+console.log(typeof bigint);
+
 let name= 'Mike';
 console.log(name);
 
-let myFavorite='food';
-food='pizza ' + 'steak';
-console.log(food);
+const faveFood = 'pizza ' + 'and' + ' steak';
+console.log(faveFood);
